b20cb6707c55d7987b1df33a7d514e2a6fc07623
Ale Uehara <you@example.com>
2013-09-19 21:10:11 -0300

    alterado o nome do projeto para cadastrousuario no settings

--- settings.py
+++ settings.py
@@ -104,7 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'cadastrousuarios.urls'
+ROOT_URLCONF = 'cadastrousuario.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
